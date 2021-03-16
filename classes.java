19cc34883eced37a1b4ac19034136b88c5cde159
kajal1sharma <kajal1231995@gmail.com>
2021-03-16 12:25:27 +0530

    16/03

---
+++ classes.java
@@ -0,0 +1,40 @@
+class Bottle{
+    int height;
+    int radius;
+    int color;
+    int capheight;
+    int capradius;
+    int volume;
+    int water;
+    public void howmuchwater(){
+        System.out.println(this.water);
+    }
+
+    
+}
+
+class Mobile{
+    int height;
+    int width;
+
+    
+}
+public class classes{
+
+    public static void main(String[] args) {
+        Bottle b1=new Bottle();
+        b1.color=1;
+        b1.radius=23;
+        b1.height=243;
+        b1.water=89;
+        b1.howmuchwater();
+        // Bottle b2=new Bottle();
+        // b2.color=15;
+        // Bottle b3=new Bottle();
+        // b3.color=15;
+        // Bottle b4=new Bottle();
+        // b4.color=15;
+        // Bottle b5=new Bottle();
+        // b5.color=15;
+    }
+} 
