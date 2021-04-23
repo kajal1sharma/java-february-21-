6beab3b2e4fedda181193278484eadfcb1f8fe50
kajal1sharma <kajal1231995@gmail.com>
2021-04-23 09:41:25 +0530

    23/4

---
+++ Abstraction.java
@@ -0,0 +1,31 @@
+interface Atm{//methods =>body
+    public void amount();
+    public void current();
+    public void savings();
+    public void deposit();
+}
+abstract class atmmachine implements Atm{
+    public void amount(){
+
+    }
+    public void savings(){
+        
+    }
+    public void current(){
+        System.out.println("hello");
+    }
+    // public void deposit(){
+        
+    // }
+}
+class abc extends atmmachine{
+    public void deposit(){
+        
+         }
+}
+class Abstraction{
+    public static void main(String[] args) {
+        abc a=new abc();
+        a.current();
+    }
+}
