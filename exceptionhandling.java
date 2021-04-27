0c557d871658ac3d8a81584e4ad1bc1404187af9
kajal1sharma <kajal1231995@gmail.com>
2021-04-27 11:59:35 +0530

    27/4

--- exceptionhandling.java
+++ exceptionhandling.java
@@ -10,14 +10,42 @@ public class exceptionhandling {
 //handle exception ,normal flow 
    
 try{
-    Abcs a=new Abcs();
-    a=null;
-    a.run(); 
-      //int a=89/0;
+    int a=25;
+    if(a==25){
+    Exception e=new Exception("iam an ecxception");
+    throw(e);
+    }else{
+        a=40;
+    }
+//    int aa=89/0;
+    // try{
+        
+    // }
+    // catch(NullPointerException n){
+
+    // }
+    // catch(ArithmeticException a){
+    //     System.out.println("arithmetic exc");
+    // }
+    // Abcs a=new Abcs();
+    // a=null;
+    // a.run(); 
+    //   //int a=89/0;
+    }
+    catch(NullPointerException obj){
+
+        System.out.println("null");
+    }
+    catch(ArithmeticException obj){
+
+        System.out.println("arith");
     }
     catch(Exception obj){
-        
+
         System.out.println(obj);
+    }
+    finally{
+        System.out.println("hello in finally");   
     }
       System.out.println("hello world");
   }  
