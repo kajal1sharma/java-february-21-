106e784cc7869aaca8792bd1c20c0bb04a58f487
kajal1sharma <kajal1231995@gmail.com>
2021-05-04 11:39:59 +0530

    4/5

--- exceptionhandling2.java
+++ exceptionhandling2.java
@@ -1,30 +1,49 @@
-//exception means =>runtime error
-//default exveption handler
 public class exceptionhandling2 {
-    public static void dofun(){
-       // try{
-            int a=90/0;//exc object
-        // }
-        // catch(Exception e){
-        //         System.out.println("this is handled "+e);
-        // }
+public static void main(String[] args) {
 
-        System.out.println("hello world");
+    //file 1 abc.txt=>char count
+    //file2 bcd.txt =>char count
+    System.out.println("first");
+    System.out.println("second");
+    try{
+    int a=90/0;
+    }catch(Exception r){
+
     }
-    public static void fun(){
-        //try{
-        dofun();
-//         }catch(Exception e){
-// System.out.println(e);
-//         }
-        System.out.println("in fun function");
-    }
-  public static void main(String[] args) {
-    try{  
-    fun();
-    }
-    catch(ArithmeticException e){
-        System.out.println(e);
-    }
-  }  
+    System.out.println("third");
+    System.out.println("second22");
+
 }
+}
+
+
+// //exception means =>runtime error
+// //default exveption handler
+// public class exceptionhandling2 {
+//     public static void dofun(){
+//        // try{
+//             int a=90/0;//exc object
+//         // }
+//         // catch(Exception e){
+//         //         System.out.println("this is handled "+e);
+//         // }
+
+//         System.out.println("hello world");
+//     }
+//     public static void fun(){
+//         //try{
+//         dofun();
+// //         }catch(Exception e){
+// // System.out.println(e);
+// //         }
+//         System.out.println("in fun function");
+//     }
+//   public static void main(String[] args) {
+//     try{  
+//     fun();
+//     }
+//     catch(ArithmeticException e){
+//         System.out.println(e);
+//     }
+//   }  
+// }
