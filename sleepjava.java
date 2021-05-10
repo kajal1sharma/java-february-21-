59543fc5fdd272c3a3c237bec326f9a9fbffe0f5
kajal1sharma <kajal1231995@gmail.com>
2021-05-10 10:02:38 +0530

    10/5

--- sleepjava.java
+++ sleepjava.java
@@ -1,3 +1,4 @@
+
 class myThread extends Thread{
     public void run(){
        // Thread.yield();
@@ -15,18 +16,37 @@ class myThread extends Thread{
         }
     }
 }
-public class joinmethod
+class myThread2 extends Thread{
+    public void run(){
+       // Thread.yield();
+        for(int i=0;i<5;i++){
+             // Thread.yield();
+             
+            System.out.println("iam a child thread 2");
+            try{
+
+             Thread.sleep(2000);
+             }
+             catch(InterruptedException e){
+                 
+             }
+        }
+    }
+}
+public class sleepjava
 {
 	public static void main(String[] args) {
 	    
 	    myThread m=new myThread();
 	    m.start();
-	    try{
-	    m.join();
-	    }
-	    catch(InterruptedException e){
+	    myThread2 m2=new myThread2();
+	    m2.start();
+	   // try{
+	   // m.join();
+	   // }
+	   // catch(InterruptedException e){
 	        
-	    }
+	   // }
 		for(int i=0;i<5;i++){
             
             System.out.println("iam a main thread");
