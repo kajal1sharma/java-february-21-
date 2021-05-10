7440c3762657dffc9526374cb4e0f21875a76ea0
kajal1sharma <kajal1231995@gmail.com>
2021-05-10 09:51:09 +0530

    10/5

--- joinmethod.java
+++ joinmethod.java
@@ -1,19 +1,34 @@
+
 class myThread extends Thread{
     public void run(){
-        Thread.yield();
-        for(int i=0;i<1000;i++){
+       // Thread.yield();
+        for(int i=0;i<5;i++){
              // Thread.yield();
+             
             System.out.println("iam a child thread");
+            try{
+
+             Thread.sleep(2000);
+             }
+             catch(Exception e){
+                 
+             }
         }
     }
 }
-public class yeildMethod
+public class joinmethod
 {
 	public static void main(String[] args) {
 	    
 	    myThread m=new myThread();
 	    m.start();
-		for(int i=0;i<1000;i++){
+	    try{
+	    m.join();
+	    }
+	    catch(Exception e){
+	        
+	    }
+		for(int i=0;i<5;i++){
             
             System.out.println("iam a main thread");
         }
