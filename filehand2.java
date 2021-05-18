62f58f3d291f994fdbf0e2c3197842084060f1ec
kajal1sharma <kajal1231995@gmail.com>
2021-05-18 11:52:10 +0530

    18/5

--- filehand2.java
+++ filehand2.java
@@ -2,15 +2,22 @@ import java.io.*;
 
 //C:\Users\ashis\Documents\javacore10ambatch\filehand2.java
 public class filehand2 {
-    public static void listfilesindir(File f){
+    public static void printlevel(int level,String name){
+        String s="";
+        for(int i=0;i<level;i++){
+            s=s+"-";
+        }
+        System.out.println(s+">"+name);
+    }
+    public static void listfilesindir(File f,int level){
         // File f=new File(ss);
         String s[]=f.list();
 
         for(int i=0;i<s.length;i++){
             File f2=new File(f,s[i]);
             if(f2.isDirectory()){
-                System.out.println(s[i]+"-------");
-                listfilesindir(f2);
+                printlevel(level,s[i]);
+                listfilesindir(f2,level+1);
             }
             else{
                 // System.out.println(s[i]);
@@ -33,8 +40,8 @@ public class filehand2 {
         for(int i=0;i<s.length;i++){
             File f2=new File(s[i]);
             if(f2.isDirectory()){
-             System.out.println(s[i]+"  ---");
-               listfilesindir(f2);
+             System.out.println("->"+s[i]);
+               listfilesindir(f2,2);
 
             }
             
